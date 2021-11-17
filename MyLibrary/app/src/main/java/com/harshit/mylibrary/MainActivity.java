5195f8061c877aac567423f3bb6922dbfc82ecb8
Harshit Verma <harshitverma14366@gmail.com>
2021-11-17 11:44:19 +0530

    new

--- MyLibrary/app/src/main/java/com/harshit/mylibrary/MainActivity.java
+++ MyLibrary/app/src/main/java/com/harshit/mylibrary/MainActivity.java
@@ -1,14 +1,36 @@
 package com.harshit.mylibrary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CardView javaCardview,pythonCardView,javaScriptCardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        javaCardview=findViewById(R.id.Java);
+        javaScriptCardView=findViewById(R.id.JavaScript);
+        pythonCardView=findViewById(R.id.Python);
+        allActions();
+    }
+
+    private void allActions() {
+        javaCardview.setOnClickListener(v -> {
+            Intent intent=new Intent(MainActivity.this,PDFViewer.class);
+            startActivity(intent);
+        });
+        pythonCardView.setOnClickListener(v->{
+            Intent intent=new Intent(MainActivity.this,PDFViewer.class);
+            startActivity(intent);
+        });
+        javaScriptCardView.setOnClickListener(v->{
+            Intent intent=new Intent(MainActivity.this,PDFViewer.class);
+            startActivity(intent);
+        });
     }
 }
